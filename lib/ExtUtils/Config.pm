f5244f7cde8bc8a5eaafaaf956a0196d66fa3916
Leon Timmermans <fawaka@gmail.com>
2011-03-21 15:31:29 +0100

    Renamed Module::Build::Config to ExtUtils::Config

--- lib/ExtUtils/Config.pm
+++ lib/ExtUtils/Config.pm
@@ -1,5 +1,97 @@
 package ExtUtils::Config;
+
 use strict;
 use warnings;
+use Config;
+
+sub new {
+	my ($pack, %args) = @_;
+	return bless {
+		stack => {},
+		values => $args{values} || {},
+	}, $pack;
+}
+
+sub get {
+	my ($self, $key) = @_;
+	return $self->{values}{$key} if ref($self) && exists $self->{values}{$key};
+	return $Config{$key};
+}
+
+sub set {
+	my ($self, $key, $val) = @_;
+	$self->{values}{$key} = $val;
+}
+
+sub push {
+	my ($self, $key, $val) = @_;
+	push @{$self->{stack}{$key}}, $self->{values}{$key} if exists $self->{values}{$key};
+	$self->{values}{$key} = $val;
+}
+
+sub pop {
+	my ($self, $key) = @_;
+
+	my $val = delete $self->{values}{$key};
+	if ( exists $self->{stack}{$key} ) {
+		$self->{values}{$key} = pop @{$self->{stack}{$key}};
+		delete $self->{stack}{$key} unless @{$self->{stack}{$key}};
+	}
+
+	return $val;
+}
+
+sub values_set {
+	my $self = shift;
+	return undef unless ref($self);
+	return $self->{values};
+}
+
+sub all_config {
+	my $self = shift;
+	my $v = ref($self) ? $self->{values} : {};
+	return {%Config, %$v};
+}
 
 1;
+
+__END__
+
+# ABSTRACT: A wrapper for perl's configuration
+
+=head1 SYNOPSYS
+
+ my $config = ExtUtils::Config->new();
+ $config->set('installsitelib', "$ENV{HOME}/lib");
+
+=head1 DESCRIPTION
+
+ExtUtils::Config is an abstraction around the %Config hash.
+
+=method new
+
+Create a new ExtUtils::Config object.
+
+=method get($key)
+
+Get the value of C<$key>. If not overriden it will return the value in %Config.
+
+=method set($key, $value)
+
+Set/override the value of C<$key> to C<$value>.
+
+=method push($key, $value)
+
+Set the value of C<$key> to C<$value> until the next C<pop> of that key.
+
+=method pop($key)
+
+Reset C<$key> to the value it had before the last C<push>.
+
+=method values_set
+
+Get a hashref of all overridden values.
+
+=method all_config
+
+Get a hashref of the complete configuration, including overrides.
